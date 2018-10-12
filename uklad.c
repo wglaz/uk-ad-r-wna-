f3b51edea13e3e3a6c7a6f16993d1e5426a52488
wglaz <wojciu6666@gmail.com>
2018-10-12 18:28:12 +0200

    zaczynam pisać

---
+++ uklad.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#define _USE_MATH_DEFINES
+
+
+#include <math.h>
+
+int main() {
+    double a1, a2, b1, b2, c1, c2, x, y;
+
+
+    printf("Podaj a1: ");
+    scanf("%lf", a1);
+    printf("Podaj b1: ");
+    scanf("%lf", b1);
+    printf("Podaj c1: ");
+    scanf("%lf", c1);
+    printf("Podaj a2: ");
+    scanf("%lf", a2);
+    printf("Podaj b2: ");
+    scanf("%lf", b2);
+    printf("Podaj c2: ");
+    scanf("%lf", c2);
+
+
+    c1 = a1*x + b1*y;
+    c2 = a2*x + b2*y;
+
+
+    return 0;
+}
+
