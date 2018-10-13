e9bbf5c22b8378702390d4a901eb5e85419d0f80
wglaz <wojciu6666@gmail.com>
2018-10-13 10:07:21 +0200

    gotowy program ... działa

--- uklad.c
+++ uklad.c
@@ -1,13 +1,15 @@
 #include <stdio.h>
-#define _USE_MATH_DEFINES
 
 
-#include <math.h>
-
-int main() {
-    double a1, a2, b1, b2, c1, c2, x, y;
 
 
+void Informacja_o_programie() {
+    printf("Program rozwiązuje układ dwóch równań liniowych.");
+    printf("\na1*x + b1*y = c1\n");
+    printf("a2*x + b2*y = c2\n");
+    printf("Podaj współczynniki.\n");
+}
+void Wprowadzanie_danych(double *c1, double *c2, double *a1, double *a2, double *b1, double *b2) {
     printf("Podaj a1: ");
     scanf("%lf", a1);
     printf("\nPodaj b1: ");
@@ -20,14 +22,65 @@ int main() {
     scanf("%lf", b2);
     printf("\nPodaj c2: ");
     scanf("%lf", c2);
+}
+void Uklad_rownan(double a1, double a2, double b1, double b2, double c1, double c2) {
+    printf("Twój układ równań: \n");
+    printf("%5.2lf*x +%5.2lf*y = %5.2lf\n", a1, b1, c1);
+    printf("%5.2lf*x +%5.2lf*y = %5.2lf\n", a2, b2, c2);
+    printf("\n");
+}
+
+double wyznacznik(double k, double l, double m, double n){
+    return k * n - l * m;
+
+}
+
+int main() {
+    double a1, a2, b1, b2, c1, c2;
+    double W, Wx, Wy;
+    double x, y;
+    Informacja_o_programie();
+    Wprowadzanie_danych(&c1, &c2, &a1, &a2, &b1, &b2);
+    Uklad_rownan(a1, a2, b1, b2, c1, c2);
+
+    W = wyznacznik(a1, a2, b1, b2);
+    Wx = wyznacznik(c1, c2, b1, b2);
+    Wy = wyznacznik(a1, a2, c1, c2);
+
+    printf("wyznacznik W= %5.2lf ", W);
+    printf("\nwyznacznik Wx= %5.2lf ", Wx);
+    printf("\nwyznacznik Wy= %5.2lf ", Wy);
 
 
-    c1 = a1*x + b1*y;
-    c2 = a2*x + b2*y;
 
-    printf("x = %lf\n", x = (c1-(b1*y))/a1);
-    printf("y")
+    if(W!=0.0){
+      x = Wx / W;
+      y = Wy / W;
+
+      printf("\nRozwiązanie:\t ");
+        printf("x = %5.2lf, y = %5.2lf", x, y);
+        printf("\n");
+
+
+    }else {
+
+    if(Wx!=0.0 || Wy!=0.0)
+    {
+    printf("\nUkład równań sprzeczny - nie ma rozwiązania. ");
+    }else
+        {
+        printf("\nUkład równań nieoznaczony - nieskończenie wiele rozwiązań.");
+    }
+}
+
+
+
+
 
     return 0;
 }
+
+
+
+
 
